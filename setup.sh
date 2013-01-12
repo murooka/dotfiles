87ea008f0c2cf8ef1f2653ed579371ff177ffb05
Naoki YAGUCHI <murooka.1129.x50@gmail.com>
2013-01-13 03:34:37 +0900

    Update 2013/1/13+

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-DOT_FILES=( .gitconfig .gitignore_global .gvimrc .vimrc .zshrc .vim )
+DOT_FILES=".gitconfig .gitignore_global .gvimrc .vimrc .zshrc .vim"
 
-for file in ${DOT_FILES[@]}
+for file in $DOT_FILES
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
