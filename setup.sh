ca1a3937ae143e1bb43d716823c8943f93289b9d
Naoki Yaguchi <murooka.1129.x50@gmail.com>
2017-03-04 18:53:44 +0900

    Add .hammerspoon

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DOT_FILES=".gemrc .gitconfig .gitignore_global .gvimrc .irbrc .tmux.conf .vim .vimrc .vrapperrc .zshrc .my.cnf .emacs.d"
+DOT_FILES=".gemrc .gitconfig .gitignore_global .gvimrc .irbrc .tmux.conf .vim .vimrc .vrapperrc .zshrc .my.cnf .emacs.d .hammerspoon"
 
 for file in $DOT_FILES
 do
